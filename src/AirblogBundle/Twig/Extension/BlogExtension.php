9dccd4d43e7d75547c461c04edd5a246a89d1c36
stefan16111@wp.pl <stefan16111@wp.pl>
2017-08-10 15:09:58 +0200

    twig extension

---
+++ src/AirblogBundle/Twig/Extension/BlogExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AirblogBundle\Twig\Extension;
+
+class BlogExtension extends \Twig_Extension {
+    
+    public function getName() {
+        return 'airblog_extension';
+    }
+    
+    public function getFunctions() {
+        return array( 
+
+        );
+    }
+    
+}
