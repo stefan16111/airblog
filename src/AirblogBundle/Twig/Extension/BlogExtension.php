e10e369c2a97cbdb5677d6986272861fd3b21926
stefan16111@wp.pl <stefan16111@wp.pl>
2017-08-11 12:33:08 +0200

    dynamic list category

--- src/AirblogBundle/Twig/Extension/BlogExtension.php
+++ src/AirblogBundle/Twig/Extension/BlogExtension.php
@@ -3,15 +3,36 @@
 namespace AirblogBundle\Twig\Extension;
 
 class BlogExtension extends \Twig_Extension {
-    
-    public function getName() {
-        return 'airblog_extension';
-    }
-    
-    public function getFunctions() {
-        return array( 
 
-        );
+    /**
+     * @var \Doctrine\Bundle\DoctrineBundle\Registry 
+     */
+    private $doctrine;
+    private $printCategoryList;
+
+    function __construct(\Doctrine\Bundle\DoctrineBundle\Registry $doctrine) {
+        $this->doctrine = $doctrine;
     }
-    
+
+    public function getFunctions() {
+        return [
+            new \Twig_SimpleFunction('categoryList', [$this, 'categoriesList'], [
+                'is_safe' => ['html'],
+                'needs_environment' => true // Tell twig we need the environment
+                    ]),
+        ];
+    }
+
+    public function categoriesList(\Twig_Environment $env) {
+        if (!isset($this->printCategoryList)) {
+            $categoryRepo = $this->doctrine->getRepository('AirblogBundle:Category');
+            $this->printCategoryList = $categoryRepo->findAll();
+        }
+        return $env->render("AirblogBundle:Template:categoriesList.html.twig", ['categoriesList' => $this->printCategoryList]);
+    }
+
+    public function getName() {
+        return 'airblog';
+    }
+
 }
