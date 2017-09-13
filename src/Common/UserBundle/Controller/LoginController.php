a799ce3084cebfc4e0842304c2863296652d73aa
stefan16111@wp.pl <stefan16111@wp.pl>
2017-09-13 19:49:28 +0200

    loginaction and security_yaml

--- src/Common/UserBundle/Controller/LoginController.php
+++ src/Common/UserBundle/Controller/LoginController.php
@@ -5,15 +5,30 @@ namespace Common\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class LoginController extends Controller
 {
     /**
-     * @Route("/login")
+     * @Route("/login", name="blog_login")
      * @Template()
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        return array();
+        $session = $this->get('session');
+        
+        if($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)){
+            $loginError = $request->attributes->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+        }else{
+            $loginError = $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        }
+        
+        $userName = $session->get(SecurityContextInterface::LAST_USERNAME);
+        
+        return [
+            'loginError' => $loginError,
+            'userName' => $userName
+            ];
     }
 }
